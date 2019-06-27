d21859dc7e7733dcdb6a062b3411685df73a8e90
oopcode <oopcode@yandex.ru>
2019-06-27 15:46:27 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# DGaming Marketplace
---
+++ x/nameservice/alias.go
@@ -0,0 +1,27 @@
+package marketplace
+
+import (
+	"github.com/cosmos/sdk-application-tutorial/x/marketplace/types"
+)
+
+const (
+	ModuleName = types.ModuleName
+	RouterKey  = types.RouterKey
+	StoreKey   = types.StoreKey
+)
+
+var (
+	NewMsgBuyName = types.NewMsgBuyName
+	NewMsgSetName = types.NewMsgSetName
+	NewWhois      = types.NewWhois
+	ModuleCdc     = types.ModuleCdc
+	RegisterCodec = types.RegisterCodec
+)
+
+type (
+	MsgSetName      = types.MsgSetName
+	MsgBuyName      = types.MsgBuyName
+	QueryResResolve = types.QueryResResolve
+	QueryResNames   = types.QueryResNames
+	Whois           = types.Whois
+)
